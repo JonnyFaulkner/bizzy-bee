2f6f8cc0b062fefe2a7ab1fa1e5cc4310c596f56
Jonathan Faulkner <83264372+JonnyFaulkner@users.noreply.github.com>
2021-10-07 23:53:59 -0500

    added search form

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import Auth from "../../utils/auth"
-import { Box, Text } from "@chakra-ui/react"
+import { Box, Text, Input, Flex, Spacer, Grid } from "@chakra-ui/react"
 import { Link } from "react-router-dom"
 
 function Nav() {
@@ -14,11 +14,23 @@ function Nav() {
     // }
 
     return (
+
         <Box w="100%" h="100px" bgGradient="linear(to-r, brand.100, brand.200, brand.100)">
-            <Link to="/">
-                <Text fontSize="6xl" >Bizzy Bee 🐝</Text>
-            </Link>
+            <Grid templateColumns="repeat(3, 1fr)" gap={8}>
+                <Box>
+                    <Link to="/">
+                        <Text fontSize="6xl" >Bizzy Bee 🐝</Text>
+                    </Link>
+                </Box>
+                <Box display="flex" alignItems="center">
+                    <Input placeholder="Search" size="lg" maxWidth="100%" bg="brand.300" />
+                </Box>
+                <Box>
+
+                </Box>
+            </Grid>
         </Box>
+
     )
 }
 
