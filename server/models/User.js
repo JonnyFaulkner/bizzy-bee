0d5b87a0562ad7fc9a1ad080665174173e9b5db4
Javier Dominguez II <Javi7057@gmail.com>
2021-10-07 22:07:44 -0500

    server running but graphql not working

--- server/models/User.js
+++ server/models/User.js
@@ -1,38 +1,31 @@
 const { Schema, model } = require("mongoose");
 const bcrypt = require("bcrypt");
 
-const userSchema = new Schema(
-  {
-    username: {
-      type: String,
-      required: true,
-      unique: true,
-      trim: true,
-    },
-    email: {
-      type: String,
-      required: true,
-      unique: true,
-      match: [/.+@.+\..+/, "Must match an email address"],
-    },
-    password: {
-      type: String,
-      required: true,
-      minlength: 6,
-    },
-    posts: [
-      {
-        type: Schema.Types.ObjectId,
-        ref: "Thought",
-      },
-    ],
+const userSchema = new Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
   },
-  {
-    toJSON: {
-      virtuals: true,
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    match: [/.+@.+\..+/, "Must match an email address"],
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 6,
+  },
+  posts: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "Post",
     },
-  }
-);
+  ],
+});
 
 // pre-save middleware to create password or modify it
 userSchema.pre("save", async function (next) {
