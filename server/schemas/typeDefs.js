c21b4a3e702a220a50d15798051ba37081ad9438
Javier Dominguez II <Javi7057@gmail.com>
2021-10-08 21:18:28 -0500

    server working and graphql working but need to add to resolvers file to get login working

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -4,10 +4,21 @@ const typeDefs = gql `
 type User {
     _id: ID
     username: String
+    email: String
+}
+
+type Auth {
+    user: User
 }
 
 type Query {
     me: User
+    users: [User]
+}
+
+type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
 }
 `;
 
