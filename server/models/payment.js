000fc248f4afd654ba3314dd9e2f687583b2e310
Rddavis1389 <rddavis1389@yahoo.com>
2021-10-09 21:28:41 -0600

    first payment cycle

--- server/models/payment.js
+++ server/models/payment.js
@@ -4,8 +4,13 @@ const stripe = require("stripe")(
   "sk_test_51Ji8YZI24J6rK8d6qHJlftYZA8EtvGOZzsEboIo42nGz0njHyiOPDuahTsqkOzDN7vREPSTkVkpOLW9lT6yAwhWk00YOTSvOnz"
 );
 
-const paymentIntent = await stripe.paymentIntents.create({
-  amount: 5000,
-  currency: 'usd',
-  payment_method_types: ['card'],
-});
+function createPaymentIntent() {
+  const paymentIntent = stripe.paymentIntents.create({
+    amount: 5000,
+    currency: "usd",
+    payment_method_types: ["card"],
+  });
+  return paymentIntent;
+}
+
+module.exports = createPaymentIntent;
--- server/server.js
+++ server/server.js
@@ -1,14 +1,12 @@
 const express = require("express");
 const path = require("path");
-// const paymentIntent = require('./models/payment')
+const createPaymentIntent = require("./models/payment");
 const env = require("dotenv").config({ path: "../.env" });
 const { ApolloServer } = require("apollo-server-express");
 
 // const { typeDefs, resolvers } = require("./schemas");
 // const db = require("./config/connection");
 const { authMiddleware } = require("./utils/auth");
-
-console.log(process.env.API_KEY);
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -36,13 +34,15 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../client/build")));
 }
 
-// app.get("*", (req, res) => {
-//   res.status(404).sendFile(path.join(__dirname, "./public/404.html"));
-// });
+// STRIPE API
 
 app.get("/secret", async (req, res) => {
-  const intent = res.json({ client_secret: intent.client_secret }); // ... Fetch or create the PaymentIntent
+  const intent = await createPaymentIntent();
+  console.log(intent);
+  res.json({ client_secret: intent.client_secret });
 });
+
+//
 
 app.get("*", (req, res) => {
   res.sendFile(__dirname, "/client/public/");
