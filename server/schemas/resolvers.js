d758b756f6ce05d59dea7ac7bc20fa57814b8ab4
Javier Dominguez II <Javi7057@gmail.com>
2021-10-12 10:30:19 -0500

    added resolvers

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -3,7 +3,17 @@ const { AuthenticationError } = require('apollo-server-express');
 const { signToken } = require('../utils/auth');
 
 const resolvers = {
-    
+    Query: {
+        me: async (parent, args, context) => {
+            if (context.user) {
+                const userData = await User.findOne({})
+                .select("-__v -password");
+
+                return userData;
+            }
+            throw new AuthenticationError("Not logged in");
+        },
+    }
 };
 
 module.exports = resolvers;
