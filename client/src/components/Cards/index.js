a98b36771fc80eaad9509e15a2b4ed29e368fa61
Jonathan Faulkner <83264372+JonnyFaulkner@users.noreply.github.com>
2021-10-14 20:01:00 -0500

    deploy to heroku

--- client/src/components/Cards/index.js
+++ client/src/components/Cards/index.js
@@ -1,6 +1,6 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 // import Image from 'next/image';
-import {Input} from "@chakra-ui/react"
+import { Input } from "@chakra-ui/react"
 import { ADD_POST } from "../../utils/mutations";
 import { useMutation } from "@apollo/client";
 import { QUERY_POSTS, QUERY_ME } from "../../utils/queries";
@@ -14,26 +14,26 @@ import {
 
 
 const PostForm = () => {
-// export default function BlogPostWithImage() {
+  // export default function BlogPostWithImage() {
   const [formText, setText] = useState('');
-  
-  const [ addPost, {error}] = useMutation(ADD_POST, {
-    update(cache, {data: {addPost}}) {
+
+  const [addPost, { error }] = useMutation(ADD_POST, {
+    update(cache, { data: { addPost } }) {
       try {
-        const {posts} = cache.readQuery({ query: QUERY_POSTS });
+        const { posts } = cache.readQuery({ query: QUERY_POSTS });
 
         cache.writeQuery({
           query: QUERY_POSTS,
-          data: { posts: [addPost, ...posts]}
+          data: { posts: [addPost, ...posts] }
         });
       } catch (e) {
         console.error(e);
       }
 
-      const {me} = cache.readQuery({query: QUERY_ME });
+      const { me } = cache.readQuery({ query: QUERY_ME });
       cache.writeQuery({
         query: QUERY_ME,
-        data: { me: {...me, posts: [...me.posts, addPost]}}
+        data: { me: { ...me, posts: [...me.posts, addPost] } }
       });
     }
   });
@@ -49,7 +49,7 @@ const PostForm = () => {
 
     try {
       await addPost({
-        variables: {formText}
+        variables: { formText }
       });
       setText('');
     } catch (e) {
@@ -60,25 +60,25 @@ const PostForm = () => {
 
   return (
     <form onSubmit={handleFormSubmit}>
-    <Stack p="4" boxShadow="lg" m="4" borderRadius="sm">
-      <Stack direction="row" alignItems="center">
-        <Text fontWeight="semibold" color="brand.300" >Post Bulletin:</Text>
-        {/* <FcLock /> */}
-      </Stack>
+      <Stack p="4" boxShadow="lg" m="4" borderRadius="sm">
+        <Stack direction="row" alignItems="center">
+          <Text fontWeight="semibold" color="brand.300" >Post Bulletin:</Text>
+          {/* <FcLock /> */}
+        </Stack>
 
-      <Stack
-        direction={{ base: 'column', md: 'row' }}
-        justifyContent="space-between">
-        <Input 
-          type='text' 
-          value={formText} 
-          onChange={handleChange} 
-        />
-        <Stack direction={{ base: 'column', md: 'row' }}>
-          <Button colorScheme="green">POST</Button>
+        <Stack
+          direction={{ base: 'column', md: 'row' }}
+          justifyContent="space-between">
+          <Input
+            type='text'
+            value={formText}
+            onChange={handleChange}
+          />
+          <Stack direction={{ base: 'column', md: 'row' }}>
+            <Button colorScheme="green">POST</Button>
+          </Stack>
         </Stack>
       </Stack>
-    </Stack>
     </form>
   );
 }
